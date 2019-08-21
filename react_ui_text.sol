0fedf738a7a82298bfb90979aa808db36e0e41b1
hoai97nam <nhnam97@gmail.com>
2019-08-21 11:59:51 +0700

    config: first change to combine reactjs

--- react_ui_text.sol
+++ react_ui_text.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.5.1;
-contract Hello {
+contract Test2 {
     
     struct usr {
         uint state;
@@ -11,8 +11,7 @@ contract Hello {
     address[] public usrStorages;
     
     string target = "https://gateway.ipfs.io/ipfs/QmUopf5TgUk9bhPSRdRtBrSS7vhhgNxWjh5mM6wHT17rev";
-    // string comp = "nam";
-    
+
     constructor() public {
         address add = msg.sender;
         usrStorage[add].state = 1;
@@ -21,25 +20,6 @@ contract Hello {
         usrStorages.push(add)-1;
     }
     
-    // function setMessage(string memory mes) public {
-    //     message = mes;
-    // }
- 
-    // function getMessage() public returns(string memory) {
-    //     // if ( stringsEqual(message, comp)){
-    //     if(keccak256(abi.encodePacked((message))) == keccak256(abi.encodePacked((comp))) ) {
-    //         return target;
-    //     }
-    //     else {
-    //          return message;
-    //     }
-    // }
-    
-    // function getMess() public returns (string memory) {
-    //     return target;
-    // }
-    
-    // ----------------------------------------------------------
     
     function confirm(address _addUsr, string memory _passwd, string memory _daytime) public returns(string memory){
         uint count = 0;
@@ -49,7 +29,11 @@ contract Hello {
                  return target;
             }
             else if ((count + 1) == uint(usrStorages.length)) {
-                addUsr(_addUsr, _passwd, _daytime);
+                usrStorage[_addUsr].state = 1;
+                usrStorage[_addUsr].usrPasswd = _passwd;
+                usrStorage[_addUsr].date = _daytime;
+                usrStorages.push(_addUsr)-1;
+                
                 return target;
             }
             else {
